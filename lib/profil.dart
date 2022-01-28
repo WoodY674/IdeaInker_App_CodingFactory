259fe9b5da69ccd8e852ee0da40e1d4f87b8011e
Saidane Ramy <saidane.ramy@gmail.fr>
2022-01-28 15:02:00 +0100

    Login (email & mdp) print

--- lib/profil.dart
+++ lib/profil.dart
@@ -8,13 +8,71 @@ class Profil extends StatefulWidget {
 }
 
 class _Profil extends State<Profil> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Text("Profil"),
-        )
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign in',
+                style: TextStyle(fontSize: 20),
+              )),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'E-mail',
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              controller: passwordController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(30, 15, 30, 0),
+              child: ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  print(nameController.text);
+                  print(passwordController.text);
+                },
+              )
+          ),
+          Row(
+            children: <Widget>[
+              const Text('Does not have account?'),
+              TextButton(
+                child: const Text(
+                  'Create an account',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  print("Coucou va créer ton compte");
+                },
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ],
+      ),
     );
   }
 }
