99875f11d2ac34de582d354e0e9e0016b8ac7b4f
Matteo95000 <61450585+Matteo95000@users.noreply.github.com>
2022-04-14 10:45:45 +0200

    get nom prenom 1

--- lib/Pages/ProfilUser.dart
+++ lib/Pages/ProfilUser.dart
@@ -1,8 +1,27 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import '../Classes/User.dart';
+import 'editUser.dart';
 
+Future<UserEdit> fetchUser() async {
+  final response = await http
+      .get(Uri.parse('http://k7-stories.com/api/users/28'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return UserEdit.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
 
 class ProfilUser extends StatefulWidget {
   static String route = 'ProfilUser';
@@ -14,6 +33,7 @@ class ProfilUser extends StatefulWidget {
 }
 
 class _ProfilUser extends State<ProfilUser> {
+  late User user = getUserInfos();
   String imagePath = "";
   final picker = ImagePicker();
 
@@ -37,8 +57,7 @@ class _ProfilUser extends State<ProfilUser> {
                     backgroundColor:
                         MaterialStateProperty.all<Color>(Colors.black)),
                 onPressed: () async {
-                  final pickedFile =
-                      await picker.pickImage(source: ImageSource.gallery);
+                  final pickedFile = await picker.pickImage(source: ImageSource.gallery);
                   // getImage à été remplacé par pickImage ?
                   if (pickedFile != null) {
                     setState(() {
@@ -50,17 +69,66 @@ class _ProfilUser extends State<ProfilUser> {
                 child: Text('Choisi une photo'),
               ),
             ),
-            imagePath != ""
-                ? Container(
+            imagePath != "" ?
+            // Affichage de l'image
+                 Container(
                     color: Colors.deepPurple,
                     //width: double.infinity,
                     padding: const EdgeInsets.symmetric(horizontal: 15),
                     child: Image.file(File(imagePath)),
                   )
-                : Container()
+                : Container(),
+
+            Container(
+              child: Text(user.firstName + user.lastName),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> getUserInfos() async {
+    final response = await http.get(
+      Uri.parse('http://k7-stories.com/api/users/28'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+
+      // If the server did return a 200,
+      // then parse the JSON.
+      Map map = json.decode(response.body);
+      setState(() {
+        user = User(map);
+        log(user.firstName);
+        log(user.lastName);
+      });
+
+      Fluttertoast.showToast(
+          msg: "Informations trouvés !",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      Navigator.pop(context);
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      Fluttertoast.showToast(
+          msg: "Recherche échouée",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    }
   }
 }
