a5100670ee0f5dcaf1bd12b7db4ebd2461ac2642
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2022-04-20 13:28:12 +0200

    update Profil user

--- lib/Pages/ProfilUser.dart
+++ lib/Pages/ProfilUser.dart
@@ -5,21 +5,66 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
-import '../Classes/User.dart';
-import 'editUser.dart';
 
-Future<UserEdit> fetchUser() async {
+import '../Classes/User.dart';
+
+Future<UserProfil> fetchUser() async {
   final response = await http
       .get(Uri.parse('http://k7-stories.com/api/users/28'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return UserEdit.fromJson(jsonDecode(response.body));
+    return UserProfil.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
+  }
+}
+
+class UserProfil {
+  final int id;
+  final String email;
+  final String password;
+  final String? adress;
+  final String? zipCode;
+  final String? city;
+  final String lastName;
+  final String firstName;
+  final String? profileImage;
+  final String? birthday;
+  final String? pseudo;
+
+
+  const UserProfil({
+    required this.id,
+    required this.email,
+    required this.password,
+    required this.adress,
+    required this.zipCode,
+    required this.city,
+    required this.lastName,
+    required this.firstName,
+    required this.profileImage,
+    required this.birthday,
+    required this.pseudo,
+  });
+
+  factory UserProfil.fromJson(Map<String, dynamic> json) {
+    return UserProfil(
+      id: json['id'],
+      email: json['email'],
+      password: json['password'],
+      adress: json['adress'],
+      zipCode: json['zipCode'],
+      city: json['city'],
+      lastName: json['lastName'],
+      firstName: json['firstName'],
+      profileImage: json['profileImage'],
+      birthday: json['birthday'],
+      pseudo: json['pseudo'],
+    );
   }
 }
 
@@ -34,7 +79,7 @@ class ProfilUser extends StatefulWidget {
 
 class _ProfilUser extends State<ProfilUser> {
   late User user;
-  late Future<User> futureUser;
+  late Future<UserProfil> futureUser;
 
   String imagePath = "";
   final picker = ImagePicker();
@@ -43,57 +88,77 @@ class _ProfilUser extends State<ProfilUser> {
   @override
   void initState() {
     super.initState();
-    futureUser = fetchUser() as Future<User>;
+    futureUser = fetchUser();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Mon profil'),
-        backgroundColor: Colors.deepPurple,
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.deepPurple,
-              padding: EdgeInsets.all(15),
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black)),
-                onPressed: () async {
-                  final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-                  // getImage à été remplacé par pickImage ?
-                  if (pickedFile != null) {
-                    setState(() {
-                      imagePath = pickedFile.path;
-                    });
-                    // Si l'image choisi n'est pas égale à null, fait un setState de imagePath = pickedFile.path;
-                  }
-                },
-                child: Text('Choisi une photo'),
-              ),
-            ),
-            imagePath != "" ?
-            // Affichage de l'image
-                 Container(
-                    color: Colors.deepPurple,
-                    //width: double.infinity,
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: Image.file(File(imagePath)),
-                  )
-                : Container(),
-
-            Container(
-              child: Text(user.firstName + user.lastName),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Mon profil'),
+          backgroundColor: Colors.deepPurple,
+          centerTitle: true,
         ),
-      ),
+        body: Container(
+            child: Column(
+                children: [
+                  Container(
+                    color: Colors.deepPurple,
+                    padding: EdgeInsets.all(15),
+                    width: double.infinity,
+                    child: FutureBuilder<UserProfil>(
+                      future: futureUser,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return ListView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            children: <Widget>[
+                              Container(
+                                child:
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                      MaterialStateProperty.all<Color>(Colors.black)),
+                                  onPressed: () async {
+                                    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+                                    // getImage à été remplacé par pickImage ?
+                                    if (pickedFile != null) {
+                                      setState(() {
+                                        imagePath = pickedFile.path;
+                                      });
+                                      // Si l'image choisi n'est pas égale à null, fait un setState de imagePath = pickedFile.path;
+                                    }
+                                  },
+                                  child: Text('Choisi une photo'),
+                                ),
+                              ),
+                              imagePath != "" ?
+                              // Affichage de l'image
+                              Container(
+                                color: Colors.deepPurple,
+                                //width: double.infinity,
+                                padding: const EdgeInsets.symmetric(horizontal: 15),
+                                child: Image.file(File(imagePath)),
+                              )
+                                  : Container(),
+
+                              Container(
+                                child: Text(snapshot.data!.firstName + snapshot.data!.lastName),
+                              ),
+                            ],
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+                        }
+                        // By default, show a loading spinner.
+                        return const CircularProgressIndicator();
+                      },
+                    ),
+                  ),
+                ],
+            ),
+        ),
     );
   }
 
