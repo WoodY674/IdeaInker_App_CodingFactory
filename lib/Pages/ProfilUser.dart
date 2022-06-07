ea8852c82915627375dac4d191f97d871fb1d088
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2022-06-07 19:06:34 +0200

    scroll ok in profil user

--- lib/Pages/ProfilUser.dart
+++ lib/Pages/ProfilUser.dart
@@ -7,12 +7,10 @@ import 'package:thebestatoo/Pages/toggleBar.dart';
 import '../Classes/User.dart';
 import 'editUser.dart';
 import 'favoritesPage.dart';
-import 'main.dart';
 
 
 class ProfilUser extends StatefulWidget {
   static String route = 'ProfilUser';
-
   const ProfilUser({Key? key}) : super(key: key);
 
   @override
@@ -39,24 +37,52 @@ class _ProfilUser extends State<ProfilUser> {
         backgroundColor: Colors.deepPurple,
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Container(
-              child: FutureBuilder<User>(
-                future: futureUser,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    user = snapshot.data!;
-                    return Container(
-                          color: Colors.deepPurple,
-                          child: Column(
-                            children: [
-                              snapshot.data!.profileImage != null ?
-                              Container(
-                                width: 200,
+      body: FutureBuilder<User>(
+        future: futureUser,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            user = snapshot.data!;
+            return NestedScrollView(headerSliverBuilder: (context, _) {
+              return [
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Container(
+                        color: Colors.deepPurple,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 10),
+                            snapshot.data!.profileImage != null ?
+                            Container(
+                              width: 200,
+                              height: 200,
+                              child: Stack(
+                                children: <Widget>[
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: IconButton(
+                                      icon: const Icon(Icons.edit),
+                                      onPressed: (){
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const EditUser()),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 200,
+                                    height: 200,
+                                    child: CircleAvatar(
+                                      backgroundImage: NetworkImage(snapshot.data!.profileImage!),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ) :
+                            Center(
+                              child: Container(
+                                width: 210,
                                 height: 200,
                                 child: Stack(
                                   children: <Widget>[
@@ -75,84 +101,58 @@ class _ProfilUser extends State<ProfilUser> {
                                     Container(
                                       width: 200,
                                       height: 200,
-                                      child: CircleAvatar(
-                                        backgroundImage: NetworkImage(snapshot.data!.profileImage!),
+                                      child: const CircleAvatar(
+                                        backgroundImage: AssetImage("noProfile.png"),
                                       ),
                                     ),
                                   ],
                                 ),
-                              ) :
-                              Center(
-                                child: Container(
-                                  width: 210,
-                                  height: 200,
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: IconButton(
-                                          icon: Icon(Icons.edit),
-                                          onPressed: (){
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) => const EditUser()),
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 200,
-                                        height: 200,
-                                        child: const CircleAvatar(
-                                          backgroundImage: AssetImage("noProfile.png"),
-                                        ),
-                                      ),
-                                    ],
+                              ),
+                            ),
+                            Container(
+                              child: Text(snapshot.data!.firstName! + " " + snapshot.data!.lastName!,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                    height: 2,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            Container(
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  ToggleBar(
+                                    labels: labels,
+                                    backgroundColor: Colors.grey.withOpacity(0.1),
+                                    backgroundBorder: Border.all(color: Colors.deepPurple),
+                                    onSelectionUpdated: (index) =>
+                                        setState(() => currentIndex = index),
                                   ),
-                                ),
+                                ],
                               ),
-                              Container(
-                                child: Text(snapshot.data!.firstName! + " " + snapshot.data!.lastName!,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      height: 2,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                              ),
-                              Container(
-                                color: Colors.white,
-                                child: Column(
-                                  children: [
-                                    ToggleBar(
-                                      labels: labels,
-                                      backgroundColor: Colors.grey.withOpacity(0.1),
-                                      backgroundBorder: Border.all(color: Colors.deepPurple),
-                                      onSelectionUpdated: (index) =>
-                                          setState(() => currentIndex = index),
-                                    ),
-                                    if(currentIndex == 0)
-                                      FavoritesPage(user)
-                                    else if(currentIndex == 1)
-                                      Informations(user)
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-
-                  } else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  }
-                  // By default, show a loading spinner.
-                  return const CircularProgressIndicator();
-                },
-              )
-            ),
-          ],
-        ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+                body: Container(
+                  child: currentIndex == 0 ?
+                          FavoritesPage(user) :
+                          Informations(user)
+                ),
+            );
+          } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
       ),
     );
   }
