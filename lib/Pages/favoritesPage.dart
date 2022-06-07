5a0f840e8cef9233106d98cc7375fd177de734ac
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2022-06-07 19:37:45 +0200

    filtered done

--- lib/Pages/favoritesPage.dart
+++ lib/Pages/favoritesPage.dart
@@ -9,7 +9,6 @@ import '../Classes/Posts.dart';
 import 'main.dart';
 
 late List<Posts> posts = [];
-late List<Posts> postsFiletred = [];
 
 class FavoritesPage extends StatelessWidget {
   final dynamic users;
@@ -19,7 +18,7 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     parsePostsImages();
     return StreamBuilder<List<Posts>>(
-      initialData: postsFiletred,
+      initialData: posts,
       builder: (BuildContext context, AsyncSnapshot<List<Posts>> snapshot) {
         return StreamBuilder<List<Posts>>(
           key: ValueKey(snapshot.data),
@@ -54,129 +53,6 @@ class FavoritesPage extends StatelessWidget {
                           style: const TextStyle(
                               fontWeight: FontWeight.w700, fontSize: 12),
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            showModalBottomSheet(
-                              backgroundColor: Colors.transparent,
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Container(
-                                  padding: const EdgeInsets.symmetric(vertical: 30),
-                                  height: MediaQuery
-                                      .of(context)
-                                      .size
-                                      .height / 2,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(35),
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      const Text(
-                                        "Share to",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 14),
-                                      ),
-                                      const SizedBox(
-                                        height: 17,
-                                      ),
-                                      SizedBox(
-                                        height: 100.0,
-                                        child: ListView.builder(
-                                          scrollDirection: Axis.horizontal,
-                                          itemCount: sharePosts.length,
-                                          itemBuilder: (context, index) {
-                                            return SizedBox(
-                                              width: 90,
-                                              child: Column(children: [
-                                                CircleAvatar(
-                                                  radius: 35,
-                                                  backgroundImage: AssetImage(
-                                                    'assets/${sharePosts[index]
-                                                        .imageUrl}',
-                                                  ),
-                                                ),
-                                                const Spacer(),
-                                                Text(
-                                                  sharePosts[index].id,
-                                                  overflow: TextOverflow.ellipsis,
-                                                  style: const TextStyle(
-                                                      fontSize: 12,
-                                                      color: Colors.black,
-                                                      fontWeight: FontWeight.w500),
-                                                )
-                                              ]),
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                      const Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            vertical: 7, horizontal: 15),
-                                        child: Divider(
-                                          color: Colors.grey,
-                                          height: 5,
-                                        ),
-                                      ),
-                                      const Spacer(),
-                                      Container(
-                                        alignment: Alignment.centerLeft,
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 15),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: const [
-                                            Text(
-                                              "This Pin was inspired by your recent activity",
-                                              style: TextStyle(fontSize: 14),
-                                              textAlign: TextAlign.left,
-                                            ),
-                                            SizedBox(
-                                              height: 20,
-                                            ),
-                                            Text(
-                                              "Hide",
-                                              style: TextStyle(
-                                                  fontSize: 19,
-                                                  fontWeight: FontWeight.w600),
-                                            ),
-                                            SizedBox(
-                                              height: 12,
-                                            ),
-                                            Text(
-                                              "Report",
-                                              style: TextStyle(
-                                                  fontSize: 19,
-                                                  fontWeight: FontWeight.w600),
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      const Spacer(),
-                                      GestureDetector(
-                                        onTap: () => Navigator.pop(context),
-                                        child: Container(
-                                          padding: const EdgeInsets.symmetric(
-                                              vertical: 20, horizontal: 26),
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                              BorderRadius.circular(30),
-                                              color: Colors.grey.shade300),
-                                          child: const Text("Close",
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.w600)),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                          child: const Icon(Icons.more_horiz),
-                        ),
                       ],
                     )
                   ],
@@ -191,43 +67,14 @@ class FavoritesPage extends StatelessWidget {
   void parsePostsImages() async {
     var postsToGet = fetchPosts();
     posts =  await postsToGet;
-    List<Posts> _filteredList = posts
-          .where((Posts user) => user.createdBy == users.id)
-          .toList();
-      _streamController.sink.add(posts);
-    postsFiletred = _filteredList;
+    _streamController.sink.add(filteredPosts(posts));
+  }
+
+  List<Posts> filteredPosts(List<Posts> posts) {
+    List<Posts> postFiltered = posts.where((element) => element.createdBy!.contains(users.id.toString())).toList();
+    return postFiltered;
   }
 }
 
 StreamController<List<Posts>> _streamController = StreamController<List<Posts>>.broadcast();
 Stream<List<Posts>> get _stream => _streamController.stream;
-
-class Post {
-  final String id;
-  final String imageUrl;
-  final String title;
-
-  Post(
-      this.id,
-      this.imageUrl,
-      this.title,
-      );
-}
-
-final List<Post> sharePosts = [
-  Post(
-    'WhatsApp',
-    'whatsapp.png',
-    '',
-  ),
-  Post(
-    'Messenger',
-    'messenger.png',
-    '',
-  ),
-  Post(
-    'Messages',
-    'messages.png',
-    '',
-  ),
-];
