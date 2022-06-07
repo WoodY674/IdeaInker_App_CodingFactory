61d77cf78c0a9258a16de6e5cfe5efb6fa4baadc
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2022-06-07 18:02:26 +0200

    add first test

---
+++ lib/Pages/main2.dart
@@ -0,0 +1,82 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:thebestatoo/Channel.dart';
+import 'package:thebestatoo/Pages/ProfilArtiste.dart';
+import 'package:thebestatoo/Pages/ProfilUser.dart';
+import 'package:thebestatoo/Pages/Avis.dart';
+import 'package:thebestatoo/Pages/addShop.dart';
+import 'package:thebestatoo/Pages/createaccount.dart';
+import 'package:thebestatoo/Pages/editUser.dart';
+import 'package:thebestatoo/Pages/listShop.dart';
+import 'package:thebestatoo/Pages/menu.dart';
+import 'package:thebestatoo/Pages/postsPage.dart';
+import 'package:thebestatoo/Pages/profil.dart';
+import 'package:thebestatoo/Pages/map.dart';
+import 'package:thebestatoo/Pages/home.dart';
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
+late final StreamingSharedPreferences preferences;
+late final String urlSite;
+late final String urlImage;
+
+Future<void> main2() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  preferences = await StreamingSharedPreferences.instance;
+  urlSite = "http://ideainker.fr/api/";
+  urlImage = "http://ideainker.fr/";
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Profil();
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late Widget currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    currentPage = const Home();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        Home.route: (context) => const Home(),
+        MyMap.route: (context) => const MyMap(),
+        CreateAccount.route: (context) => const CreateAccount(),
+        Menu.route: (context) => const Menu(),
+        EditUser.route: (context) => const EditUser(),
+        AddShop.route: (context) => const AddShop(),
+        ListShop.route: (context) => const ListShop(),
+        ProfilUser.route: (context) => const ProfilUser(),
+        ProfilArtiste.route: (context) => const ProfilArtiste(),
+        Profil.route: (context) => const Profil(),
+        Channel.route: (context) => Channel(),
+        PostsPage.route:(context) => const PostsPage(),
+      },
+      home: Scaffold(
+        body: currentPage,
+      ),
+    );
+  }
+}
+
+Future<void> Disconnect() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  preferences.setString('token', '');
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:thebestatoo/Pages/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Début du test login', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
