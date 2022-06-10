9f48922501c7f1fb43ce309ab72178855cddb506
antoine <antoine-haller@outlook.fr>
2022-06-10 10:07:50 +0200

    Update Channel.dart

--- lib/Channel.dart
+++ lib/Channel.dart
@@ -5,6 +5,7 @@ import 'package:thebestatoo/chat/models/chatMessage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+//test
 
 String token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NTIyNTQ1MDAsImV4cCI6MTY1MjI4NTEwMCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoiYW50b2luZS1oYWxsZXJAb3V0bG9vay5mciJ9.Osp_-MS6gpkBLlZXglbLglQynoeJTn4kkrUkMhi3WmuvTlLtK5uEKx-u6eEr13P9YWK2E0gMn4MRiq-8PNb_VPTgRHW0Uo5DVvyAbIt1V7V7QYJe-mSSoyApzdSaay3lDcrfOF3q--LqEAV1UNq7KL6wfInL8w47oNUK7EAGbBwk25331p_iAgW-h7nspj4y0sZU3ln8YnBczV_7ADLZ2jL7fiRV_r-J1m9-eEhhYQ9FHsRFG5EdyXag_cEsCAM-BrFDERwiTaO2JDTVHcZsBMo9XBeXvPSNEn63Fdk2Mh-6HudtpI5k0JJSDAj14UE0h2H91O0C87fK0v3fIWyppg';
 
